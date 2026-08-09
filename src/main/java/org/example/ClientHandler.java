c507ff48ada3efb1d60f22688c70f21ffb06f034
utkashu281 <utkarshu888@gmail.com>
2026-08-09 21:46:01 +0530

    fixed server output

--- src/main/java/org/example/ClientHandler.java
+++ src/main/java/org/example/ClientHandler.java
@@ -26,6 +26,7 @@ public class ClientHandler {
     }
     public int parsingMethod(ArrayList<String> arr){
         String[] arrOfMethods=arr.get(0).split(" ");
+        parsingBody(arr);
         return switch (arrOfMethods[0]) {
             case "GET" -> 200;
             case "POST" -> 405;
@@ -43,11 +44,28 @@ public class ClientHandler {
         }
         //printingValue();
     }
-    public void printingValue(){
-        httpValue.forEach((key,value)-> System.out.println(key+":"+value));
+    public void printingValue(PrintWriter out){
+        httpValue.forEach((key,value)-> out.println(key+":"+value));
         httpValue.clear();
     }
     public void serverOutput(PrintWriter output,int statusCode){
-        System.out.printf("HTTP/1.1 %d OK\nHello from http server\n",statusCode);
+        String content="Hello from java HTTP server developed by utkarsh upadhyay\n";
+        int contentLength=content.length();
+        String response;
+        if(statusCode==200){
+            response="OK";
+            output.printf("HTTP/1.1 %d %s\r\n",statusCode,response);
+        } else if (statusCode==405) {
+            response="Method Not Allowed";
+            output.printf("HTTP/1.1 %d %s\r\n",statusCode,response);
+        } else if (statusCode==404) {
+            response="Not Found";
+            output.printf("HTTP/1.1 %d %s\r\n",statusCode,response);
+        }
+        output.printf("Content-Type: text/plain\r\n");
+        output.printf("Content-Length: %d\r\n",contentLength);
+        //a mandatory blank line to separate headers from the body
+        output.print("\r\n");
+        output.print(content);
     }
 }
